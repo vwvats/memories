69e146cbdcd9d67c41f5bf447459f36576bfc1af
VW Vats <vwvats@gmail.com>
2021-11-26 23:12:44 +0530

    built the form UI to create posts

--- client/src/App.js
+++ client/src/App.js
@@ -23,7 +23,7 @@ const App = () => {
                 <img className={classes.image} src={memories} alt="memories" height="60" />
             </AppBar>
             <Grow in>
-                <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
                     <Grid item xs={12} sm={7}>
                         <Posts />
                     </Grid>
